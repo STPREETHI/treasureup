1367d983c0a732d2314b93b23e3ea1098ae2de5b
STPREETHI <158798657+STPREETHI@users.noreply.github.com>
2025-11-25 11:42:46 +0530

    Update firebaseCredentials.ts

--- firebaseCredentials.ts
+++ firebaseCredentials.ts
@@ -8,14 +8,12 @@
 // 5. Copy the entire object and paste it here, replacing the placeholder values.
 // ====================================================================================
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyDkKWp1ir3hMj-5MdgIWTYt35wRmRWWf9Q",
-  authDomain: "treasuryapp-4d288.firebaseapp.com",
-  projectId: "treasuryapp-4d288",
-  storageBucket: "treasuryapp-4d288.firebasestorage.app",
-  messagingSenderId: "613080893235",
-  appId: "1:613080893235:web:04b475ef883fbef5526046",
-  measurementId: "G-78NTWKEMYZ"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
